6a33f706e109e2054a196c4ffced77d9b5e93f2c
Ubuntu <ubuntu@ip-172-31-22-78.ca-central-1.compute.internal>
2020-09-09 22:15:25 +0000

    secrets work

--- change_and_upload.sh
+++ change_and_upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo $(date) >> important_info.txt
 
+source secrets.sh
+
 array[0]="some minor updates"
 array[1]="changed file structure"
 array[2]="cleaning..."
@@ -12,5 +14,5 @@ index=$(($RANDOM % $size))
 git stage "important_info.txt"
 git commit -m "${array[$index]}"
 
-git push
+git push https://$user:$pass@github.com/EliBildman/TotallyRealProject.git
 
