6bff983f6d1759e194f6a926df2105e7d98551a9
Ubuntu <ubuntu@ip-172-31-22-78.ca-central-1.compute.internal>
2020-09-09 22:43:33 +0000

    added randomness

--- change_and_upload.sh
+++ change_and_upload.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-echo $(date) >> important_info.txt
 
-source secrets.sh
+rand=$(( $RANDOM % 3 ))
 
-array[0]="some minor updates"
-array[1]="changed file structure"
-array[2]="cleaning..."
-array[3]="major revamp"
+if [ $rand -ge 1 ]
+then
 
-size=${#array[@]}
-index=$(($RANDOM % $size))
+    echo "last ran $(date)" > important_info.txt
 
-git stage "important_info.txt"
-git commit -m "${array[$index]}"
+    source secrets.sh
 
-git push https://$user:$pass@github.com/EliBildman/TotallyRealProject.git
+    array[0]="some minor updates"
+    array[1]="changed file structure"
+    array[2]="cleaning..."
+    array[3]="major revamp"
 
+    size=${#array[@]}
+    index=$(($RANDOM % $size))
+
+    git stage "important_info.txt"
+    git commit -m "${array[$index]}"
+
+    git push https://$user:$pass@github.com/EliBildman/TotallyRealProject.git
+else
+
+    echo "did not run"
+
+fi
